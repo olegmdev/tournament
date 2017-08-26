759718dc05b95314b4bbe6dee9e3b286ea817352
Oleg <qu1ze34@gmail.com>
2017-08-26 23:00:33 +0300

    fixed router.go formatting

--- server/router.go
+++ server/router.go
@@ -8,34 +8,34 @@ import (
 func Router() *gin.Engine {
   router := gin.Default()
 
-	v1 := router.Group("v1")
-	{
-    playerGroup := v1.Group("players")
+    v1 := router.Group("v1")
     {
-      player := new(controllers.PlayerController)
-      playerGroup.POST("/fund", player.Fund)
-      playerGroup.POST("/take", player.Take)
-      playerGroup.GET("/:id/balance", player.Balance)
+      playerGroup := v1.Group("players")
+      {
+        player := new(controllers.PlayerController)
+        playerGroup.POST("/fund", player.Fund)
+        playerGroup.POST("/take", player.Take)
+        playerGroup.GET("/:id/balance", player.Balance)
+      }
+
+      tournamentGroup := v1.Group("tournaments")
+      {
+        tournament := new(controllers.TournamentController)
+        tournamentGroup.POST("/announce", tournament.Announce)
+        tournamentGroup.POST("/result", tournament.Result)
+        tournamentGroup.POST("/join", tournament.Join)
+      }
+
+      systemGroup := v1.Group("system")
+      {
+        system := new(controllers.SystemController)
+        systemGroup.DELETE("/reset", system.Reset)
+      }
     }
 
-    tournamentGroup := v1.Group("tournaments")
-    {
-      tournament := new(controllers.TournamentController)
-      tournamentGroup.POST("/announce", tournament.Announce)
-      tournamentGroup.POST("/result", tournament.Result)
-      tournamentGroup.POST("/join", tournament.Join)
-    }
+    router.NoRoute(func(c *gin.Context) {
+      c.AbortWithStatus(404)
+    })
 
-    systemGroup := v1.Group("system")
-    {
-      system := new(controllers.SystemController)
-      systemGroup.DELETE("/reset", system.Reset)
-    }
-	}
-
-	router.NoRoute(func(c *gin.Context) {
-		c.AbortWithStatus(404)
-	})
-
-	return router
+    return router
 }
