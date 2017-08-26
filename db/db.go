297b3714d8f929e82f09bb8da218d68fb35656bc
Oleg <qu1ze34@gmail.com>
2017-08-26 23:03:53 +0300

    fixed formatting in db.go

--- db/db.go
+++ db/db.go
@@ -1,11 +1,10 @@
 package db
 
 import (
-	"fmt"
-	"tournament/config"
-
+  "fmt"
+  "tournament/config"
   _ "github.com/lib/pq"
-	"github.com/mattes/migrate"
+  "github.com/mattes/migrate"
   "github.com/mattes/migrate/database/postgres"
   _ "github.com/mattes/migrate/source/file"
   "github.com/jinzhu/gorm"
@@ -15,23 +14,23 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	var err error
-	config := config.GetConfig()
+  var err error
+  config := config.GetConfig()
 
-	if DB, err = gorm.Open("postgres", config.GetString("database")); err != nil {
-		panic(fmt.Sprintf("No error should happen when connecting to database, but got err=%+v", err))
-	}
+  if DB, err = gorm.Open("postgres", config.GetString("database")); err != nil {
+    panic(fmt.Sprintf("No error should happen when connecting to database, but got err=%+v", err))
+  }
 
-	// Run database migrations
-	driver, err := postgres.WithInstance(DB.DB(), &postgres.Config{})
-	migrator, err := migrate.NewWithDatabaseInstance("file://db/migrations", "postgres", driver)
+  // Run database migrations
+  driver, err := postgres.WithInstance(DB.DB(), &postgres.Config{})
+  migrator, err := migrate.NewWithDatabaseInstance("file://db/migrations", "postgres", driver)
   migrator.Up()
 }
 
 func GetDB() *gorm.DB {
-	return DB
+  return DB
 }
 
 func Lock(instance *gorm.DB) *gorm.DB {
-	return instance.Set("gorm:query_option", "FOR UPDATE")
+  return instance.Set("gorm:query_option", "FOR UPDATE")
 }
