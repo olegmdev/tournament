3a2ec13afa9efe73723eb1956ec12f30ab1ab1aa
Oleg <qu1ze34@gmail.com>
2017-08-26 23:04:54 +0300

    fixed formatting in config.go

--- config/config.go
+++ config/config.go
@@ -8,19 +8,19 @@ import (
 var config *viper.Viper
 
 func Init(env string) {
-	var err error
+  var err error
 
   config = viper.New()
-	config.SetConfigType("yaml")
-	config.SetConfigName(env)
-	config.AddConfigPath("config/")
+  config.SetConfigType("yaml")
+  config.SetConfigName(env)
+  config.AddConfigPath("config/")
 
   err = config.ReadInConfig()
-	if err != nil {
-		log.Fatal("error on parsing configuration file")
-	}
+  if err != nil {
+    log.Fatal("error on parsing configuration file")
+  }
 }
 
 func GetConfig() *viper.Viper {
-	return config
+  return config
 }
